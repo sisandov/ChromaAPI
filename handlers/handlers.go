e3f8ea7264121d7fc1b5ed06802b754d36042574
alvaro14s <escalona.alvaro.13@gmail.com>
2024-04-16 10:36:30 -0400

    9-handlers: Add functions for generate random number valid for rgb and transform that to hex to generate random colors

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,39 +2,61 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
+	"math/rand/v2"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 func RootHandler(w http.ResponseWriter, r *http.Request) {
-    jsonResponse := map[string]string{
-        "message": "Hello, world!",
-    }
+	jsonResponse := map[string]string{
+		"message": "Hello, world!",
+	}
 
-    jsonResponseBytes, err := json.Marshal(jsonResponse)
-    if err != nil {
-        http.Error(w, "Internal server error", http.StatusInternalServerError)
-        return
-    }
-
-    w.Header().Set("Content-Type", "application/json")
-
-    w.WriteHeader(http.StatusOK)
-    w.Write(jsonResponseBytes)
+	CommonResponse(w, r, jsonResponse)
 }
 
-func RandomHandler(w http.ResponseWriter, r *http.Request) {
-    jsonResponse := map[string]string{
-        "message": "Generating random color...",
-    }
+const totalNumbers = 255
 
-    jsonResponseBytes, err := json.Marshal(jsonResponse)
-    if err != nil {
-        http.Error(w, "Internal server error", http.StatusInternalServerError)
-        return
-    }
+func generateRandomRGBNumber() int64 {
+	return int64(rand.IntN(totalNumbers))
+}
 
-    w.Header().Set("Content-Type", "application/json")
+func CommonResponse(w http.ResponseWriter, r *http.Request, jsonResponse map[string]string) {
+	jsonResponseBytes, err := json.Marshal(jsonResponse)
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
 
-    w.WriteHeader(http.StatusOK)
-    w.Write(jsonResponseBytes)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonResponseBytes)
+}
+
+func generateRandomHex() string {
+	generatedHexNumber := strings.ToUpper(strconv.FormatInt(generateRandomRGBNumber(), 16))
+	return strings.Repeat("0", 2-len(generatedHexNumber)) + generatedHexNumber
+}
+
+func RandomHEXHandler(w http.ResponseWriter, r *http.Request) {
+	jsonResponse := map[string]string{
+		"color": fmt.Sprintf("#%s%s%s", generateRandomHex(), generateRandomHex(), generateRandomHex()),
+	}
+	CommonResponse(w, r, jsonResponse)
+}
+
+func RandomRGBHandler(w http.ResponseWriter, r *http.Request) {
+	red := fmt.Sprint(generateRandomRGBNumber())
+	green := fmt.Sprint(generateRandomRGBNumber())
+	blue := fmt.Sprint(generateRandomRGBNumber())
+
+	jsonResponse := map[string]string{
+		"color": fmt.Sprintf("(%s, %s, %s)", red, green, blue),
+		"red":   red,
+		"green": green,
+		"blue":  blue,
+	}
+	CommonResponse(w, r, jsonResponse)
 }
