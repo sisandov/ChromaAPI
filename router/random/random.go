4e50ad91ed1ef5e88db848c234b31baa8f6ad9bc
alvaro14s <escalona.alvaro.13@gmail.com>
2024-04-16 10:37:02 -0400

    9-router/random: Add router for random hex and random rgb colors

--- router/random/random.go
+++ router/random/random.go
@@ -7,10 +7,9 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-
-
-func Router() http.Handler { 
+func Router() http.Handler {
 	r := chi.NewRouter()
-	r.Get("/random", handlers.RandomHandler)
+	r.Get("/random/hex", handlers.RandomHEXHandler)
+	r.Get("/random/rgb", handlers.RandomRGBHandler)
 	return r
 }
