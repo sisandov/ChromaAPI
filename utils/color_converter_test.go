1ae55d71f3245778bc2e0d6c1072a537a8b45984
alvaro14s <escalona.alvaro.13@gmail.com>
2024-04-18 11:53:51 -0400

    16-utils:  Add test for hsl converters

--- utils/color_converter_test.go
+++ utils/color_converter_test.go
@@ -2,6 +2,7 @@ package color_converter_test
 
 import (
 	color_converter "chroma-api/utils"
+	"fmt"
 	"testing"
 )
 
@@ -19,4 +20,29 @@ func TestHEXToRGB(t *testing.T) {
 		t.Fatalf("Error in the color converter: %s", colorError.Error())
 	}
 	println(color)
+}
+
+func TestRGBToHSL(t *testing.T) {
+	color, colorError := color_converter.RGBToHSL("rgb(123, 123, 123)")
+	if colorError != nil {
+		t.Fatalf("Error in the RGBToHSL converter: %s", colorError.Error())
+	}
+	println(color)
+}
+
+func TestHEXToHSL(t *testing.T) {
+	colorHSL, colorError := color_converter.HEXToHSL("#AAAAAA")
+	if colorError != nil {
+		t.Fatalf("Error in the RGBToHSL converter: %s", colorError.Error())
+	}
+	println(colorHSL)
+}
+
+func TestParseRGB(t *testing.T) {
+	rgbSlice := color_converter.ParseRGB("rgb(1,2,3)")
+	if len(rgbSlice) != 3 {
+		t.Fatalf("Error on slice %v", rgbSlice)
+	}
+
+	println(fmt.Sprintf("%v", rgbSlice))
 }
